eacdea84d1844e6fd8875a907141e557458352ad
noplur <geoduckphoto@gmail.com>
2021-07-12 15:23:23 -0700

    modified UI components to render images

--- deep-thoughts/server/utils/params-config.js
+++ deep-thoughts/server/utils/params-config.js
@@ -4,10 +4,17 @@ const params = fileName => {
     const myFile = fileName.originalname.split('.');
     const fileType = myFile[myFile.length - 1];
   
+    // const imageParams = {
+    //   Bucket: 'user-images-7d60049c-1817-445d-88a6-0a5cd155b06b',
+    //   Key: `${uuidv4()}.${fileType}`,
+    //   Body: fileName.buffer,
+    // };
+
     const imageParams = {
       Bucket: 'user-images-7d60049c-1817-445d-88a6-0a5cd155b06b',
       Key: `${uuidv4()}.${fileType}`,
       Body: fileName.buffer,
+      ACL: 'public-read' // allow read access to this file
     };
   
     return imageParams;
